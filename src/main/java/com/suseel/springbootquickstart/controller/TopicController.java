f88377d8a07a721291efded97bd5b09aaf73280f
sbam1 <bamsuseel@gmail.com>
2017-05-06 20:12:06 -0400

    adding topic controller to return all the topics

---
+++ src/main/java/com/suseel/springbootquickstart/controller/TopicController.java
@@ -0,0 +1,17 @@
+package com.suseel.springbootquickstart.controller;
+
+import com.suseel.springbootquickstart.model.Topic;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topics.add(new Topic(1, "spring mvc", "this is a spring mvc tutorial"));
+        return topics;
+    }
+}
