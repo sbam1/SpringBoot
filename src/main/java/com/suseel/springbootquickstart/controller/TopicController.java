95426247d427b914128a165e40f7fd78283d99dd
sbam1 <bamsuseel@gmail.com>
2017-05-06 20:21:17 -0400

    adding topic controller to get all topics

--- src/main/java/com/suseel/springbootquickstart/controller/TopicController.java
+++ src/main/java/com/suseel/springbootquickstart/controller/TopicController.java
@@ -1,6 +1,7 @@
 package com.suseel.springbootquickstart.controller;
 
 import com.suseel.springbootquickstart.model.Topic;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         List<Topic> topics = new ArrayList<>();
         topics.add(new Topic(1, "spring mvc", "this is a spring mvc tutorial"));
